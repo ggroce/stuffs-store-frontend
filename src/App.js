bacde095911097776b0245a87a9fd52b92510b28
ggroce <ggrocedev@gmail.com>
2021-07-25 19:08:45 -0500

    added ShopPage and shop page data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage.jsx";
+import ShopPage from "./pages/shop/ShopPage.jsx";
 import { Switch, Route } from "react-router";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/ShopPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SHOP_DATA from "./ShopPage.data.js";
+
+const ShopPage = () => {
+  return (
+    <div>
+      <h1>Shop</h1>
+    </div>
+  );
+};
+export default ShopPage;
