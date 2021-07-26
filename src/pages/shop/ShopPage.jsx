6f85cf14efb66ebbf5eb851556ba3de8f187ea42
ggroce <ggrocedev@gmail.com>
2021-07-25 20:46:32 -0500

    added collections under shop page

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import SHOP_DATA from "./ShopPage.data.js";
+import CollectionPreview from "../../components/collection-preview/collection-preview.jsx";
 
 const ShopPage = () => {
   return (
-    <div>
-      <h1>Shop</h1>
+    <div className="shop-page">
+      {SHOP_DATA.map(({ id, ...otherProps }) => (
+        <CollectionPreview key={id} {...otherProps} />
+      ))}
     </div>
   );
 };
