c8c691a33be27698377c5b6f303ecac907790858
Garrett Groce <ggroce@gmail.com>
2021-07-27 13:53:26 -0500

    added header and header styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
+import Header from './components/header/header.jsx'
 import HomePage from './pages/homepage/HomePage.jsx'
 import ShopPage from './pages/shop/ShopPage.jsx'
 import { Switch, Route } from 'react-router'
@@ -7,6 +8,7 @@ import { Switch, Route } from 'react-router'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
