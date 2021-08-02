cd48578d357c04705901ae1a6a452c51f316fb64
ggroce <ggrocedev@gmail.com>
2021-08-01 19:30:43 -0500

    added CartItem component, which show up in CartDropdown component when items added to cart

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './CartDropdown.styles.scss'
 import CustomButton from '../CustomButton/CustomButton.jsx'
+import CartItem from '../CartItem/CartItem.jsx'
 import { useSelector } from 'react-redux'
 
 const CartDropdown = () => {
+  const cart = useSelector((state) => state.cart.cartItems)
+
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cart.map((item) => {
+          return <CartItem key={item.id} item={item} />
+        })}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   )
