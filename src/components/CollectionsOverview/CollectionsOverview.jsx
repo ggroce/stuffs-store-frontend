a6ce8904aa3a8483512607c7be3dd08266fac2b5
ggroce <ggrocedev@gmail.com>
2021-08-04 21:43:59 -0500

    added new selectCollectionsForPreview shop data selector to return an array after old one was refactored to return an object, fixing bug with CollectionOverview

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './CollectionsOverview.styles.scss'
 import { useSelector } from 'react-redux'
-import { selectCollections } from '../../redux/shop/shop.selectors.js'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors.js'
 import CollectionPreview from '../../components/CollectionPreview/CollectionPreview.jsx'
 
 const CollectionsOverview = () => {
-  const shopData = useSelector(selectCollections)
+  const shopData = useSelector(selectCollectionsForPreview)
 
   return (
     <div className="collections-overview">
