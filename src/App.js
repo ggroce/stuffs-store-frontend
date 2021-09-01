1a9bc7524deb0fc25c0c4921819b55aaf164dcc6
ggroce <ggrocedev@gmail.com>
2021-09-01 00:39:58 -0500

    icon attribution

--- src/App.js
+++ src/App.js
@@ -45,6 +45,16 @@ function App() {
           <Route exact path="/checkout" component={CheckoutPage} />
           <Route component={NotFound} />
         </Switch>
+        <div>
+          Icons made by{" "}
+          <a href="https://www.freepik.com" title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
       </Suspense>
     </>
   );
