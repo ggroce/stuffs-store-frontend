240a3f87a075d3ca90c46ca7e589539d4e23cf73
ggroce <ggrocedev@gmail.com>
2021-09-08 22:24:32 -0500

    finished changes to shop.data.js and directory.data.js

--- src/redux/directory/directory.data.js
+++ src/redux/directory/directory.data.js
@@ -1,35 +1,55 @@
 const DIRECTORY_DATA = [
   {
-    title: "hats",
-    imageUrl: "images/hats.png",
+    title: "coffee makers",
+    imageUrl: "images/directory/coffee-makers.jpg",
     id: 1,
-    linkUrl: "shop/hats",
+    linkUrl: "shop/coffeemakers",
+    attribution: {
+      photagrapher: "Caleb Dow",
+      link: "https://unsplash.com/s/photos/coffee-makers?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    },
   },
   {
-    title: "jackets",
-    imageUrl: "images/jackets.png",
+    title: "coffee grinders",
+    imageUrl: "images/directory/coffee-grinders.jpg",
     id: 2,
-    linkUrl: "shop/jackets",
+    linkUrl: "shop/coffeegrinders",
+    attribution: {
+      photagrapher: "Declan Cronin",
+      link: "https://unsplash.com/@dipaccicoffeeco?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    },
   },
   {
-    title: "sneakers",
-    imageUrl: "images/sneakers.png",
+    title: "coffee thermoses",
+    imageUrl: "images/directory/coffee-thermoses.jpg",
     id: 3,
-    linkUrl: "shop/sneakers",
+    linkUrl: "shop/coffeethermoses",
+    attribution: {
+      photagrapher: "Daniel Hooper",
+      link: "https://unsplash.com/@dan_fromyesmorecontent?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    },
   },
   {
-    title: "womens",
-    imageUrl: "images/womens.png",
+    title: "ceramic mugs",
+    imageUrl: "images/directory/ceramic-mugs.jpg",
     size: "large",
     id: 4,
-    linkUrl: "shop/womens",
+    linkUrl: "shop/ceramicmugs",
+    attribution: {
+      photagrapher: "Annie Spratt",
+      link: "https://unsplash.com/@anniespratt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    },
   },
   {
-    title: "mens",
-    imageUrl: "images/men.png",
+    title: "caffinated beans",
+    imageUrl: "images/directory/caffinated-beans.jpg",
     size: "large",
     id: 5,
-    linkUrl: "shop/mens",
+    linkUrl: "shop/caffinatedbeans",
+    attribution: {
+      photagrapher: "Nathan Dumlao",
+      link: "https://unsplash.com/s/photos/coffee-beans?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+    },
   },
 ];
 
